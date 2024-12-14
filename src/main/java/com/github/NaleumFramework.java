0536b4bb48e6caa3f9a78146c03b12d95a67e680
HyeonJae07 <lis131017@gmail.com>
2024-12-15 05:09:01 +0900

    commit

---
+++ src/main/java/com/github/NaleumFramework.java
@@ -0,0 +1,15 @@
+package com.github;
+
+import com.github.utils.CL;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NaleumFramework extends JavaPlugin {
+    private static NaleumFramework instance;
+
+    @Override
+    public void onEnable() {
+        if (instance != null) { return; }
+        CL.Send("&9Naleum&bFrameWork &7Enable", this);
+        CL.Send("&7Support Discord &8: &9https://discord.gg/XbxTxdf7jh", this);
+    }
+}
