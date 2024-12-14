b4d1a35a0e6c26c44f6b17f51c88db33a5fbb2b4
HyeonJae07 <lis131017@gmail.com>
2024-12-14 20:02:26 +0900

    Initial commit

---
+++ src/main/java/NaLeum/naleumFramework/NaleumFramework.java
@@ -0,0 +1,10 @@
+package NaLeum.naleumFramework;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NaleumFramework extends JavaPlugin {
+
+    public static String test(String text) {
+        return "test" + text;
+    }
+}
