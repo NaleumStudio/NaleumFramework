474f2c29ed0e555546ff57bdaa700089632062b2
HyeonJae07 <lis131017@gmail.com>
2024-12-16 20:51:41 +0900

    ConfigSetting

--- src/main/java/com/github/PluginConfig.java
+++ src/main/java/com/github/PluginConfig.java
@@ -1,11 +1,9 @@
 package com.github;
 
-import java.util.HashMap;
-
 public class PluginConfig {
     public static boolean enableDB;
 
-    public void ReloadConfig() {
+    public static void ReloadConfig() {
         NaleumFramework.getInstance().reloadConfig();
         enableDB = NaleumFramework.getInstance().getConfig().getBoolean("database.enabled", true);
     }
