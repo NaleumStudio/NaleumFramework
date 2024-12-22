09653ce7d92d7a55e8befb8a4017237416ba89e4
HyeonJae07 <lis131017@gmail.com>
2024-12-23 00:12:54 +0900

    Update

--- src/main/java/com/github/NaleumFramework.java
+++ src/main/java/com/github/NaleumFramework.java
@@ -16,7 +16,6 @@ public final class NaleumFramework extends JavaPlugin {
         CL.Send("&9Naleum&bFrameWork &7Enable", this);
         CL.Send("&7Support Discord &8: &9https://discord.gg/XbxTxdf7jh", this);
         FILE.createFolderINE(this.getDataFolder());
-        FILE.createFolderINE(new File(this.getDataFolder(), "DB"));
         PluginConfig.ReloadConfig();
     }
 
